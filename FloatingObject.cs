7c28739afc9eaaf7b7f476edda69ac3ee60ed242
HeavensGates <37469524+HeavensGates@users.noreply.github.com>
2020-11-23 12:59:27 +0100

    Floating Object
    An object that floats around player and goes boom on impact, go wacky with values

---
+++ FloatingObject.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingObject : MonoBehaviour
+{
+    private Transform playerTransform;
+    private Rigidbody rb;
+
+    [SerializeField] float idleMoveSpeed=1;
+
+    private bool didHit = false;
+    private Vector3 rotationAxis = Vector3.up;
+    private Vector3 collisionVelocity;
+    
+    [SerializeField] private float forceMultiplier = 10;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Debug.Log(didHit);
+        if (!didHit)
+        {
+            transform.RotateAround(playerTransform.position, rotationAxis, idleMoveSpeed * Time.deltaTime);
+        }
+    }
+    private void CalculateCollisionForce()
+    {
+        Vector3 offsetFromCenter = transform.position - playerTransform.position;
+        float radius = offsetFromCenter.magnitude;
+
+        Vector3 travelDirection = Vector3.Cross(rotationAxis, offsetFromCenter).normalized;
+
+        collisionVelocity = radius * idleMoveSpeed * Mathf.Deg2Rad * travelDirection;
+
+        rb.velocity = collisionVelocity * forceMultiplier;
+
+        didHit = true;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!didHit)
+        {
+            CalculateCollisionForce();
+        }
+    }
+}
