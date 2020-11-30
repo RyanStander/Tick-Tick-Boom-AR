25f6b8c19be97d9a810d15f816c82e10ad0f8e3d
JacquesAV <JacquesAV@users.noreply.github.com>
2020-11-30 12:34:14 +0100

    Created ToggleActiveState
    Functions for an array of objects.

---
+++ ToggleActiveState.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleActiveState : MonoBehaviour
+{
+    [SerializeField] private GameObject[] toggleObjectsOn = null, toggleObjectsOff = null;
+    public void ToggleObjects()
+    {
+        if(toggleObjectsOff!=null)
+        {
+            foreach (GameObject singleObject in toggleObjectsOff)
+            {
+                singleObject.SetActive(false);
+            }
+        }
+        if(toggleObjectsOn!=null)
+        {
+            foreach (GameObject singleObject in toggleObjectsOn)
+            {
+                singleObject.SetActive(true);
+            }
+        }
+    }
+}
